af4b2cb7d5241dcfdc584442349662d9a40af36f
Dmitry globa <dima.globa2015@yandex.ru>
2026-09-20 23:25:16 +0800

    feat: implement projects table creation

--- src/ai_assistant/core/database.py
+++ src/ai_assistant/core/database.py
@@ -8,3 +8,17 @@ def get_db_connection(db_path: str) -> sqlite3.Connection:
     conn.row_factory = sqlite3.Row
 
     return conn
+
+
+def create_tables(conn: sqlite3.Connection):
+    """Функция для создания таблицы, с проектами"""
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS projects (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        path TEXT NOT NULL UNIQUE,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+                    """)
+    conn.commit()
